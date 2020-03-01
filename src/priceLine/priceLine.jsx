a82f8352680b1b98770cf80ca74ffacbdf638371
adringue <adrien.nguesseu@gmail.com>
2020-03-01 10:46:53 -0800

    added Navbar Icon

--- src/priceLine/priceLine.jsx
+++ src/priceLine/priceLine.jsx
@@ -10,8 +10,10 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import PriceLineMain from "../priceLine-main/priceLineMain";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import {faHome, faLuggageCart, faQuestion} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-regular-svg-icons";
+import {faQuestionCircle} from "@fortawesome/free-regular-svg-icons/faQuestionCircle";
 
 
 
@@ -54,22 +56,47 @@ class PriceLine extends React.Component {
                                                 <Nav.Link href="#features">Cruises</Nav.Link>
                                             </Nav>
                                             <Nav>
-                                                <Nav.Link href="#deets">  <NavDropdown className="adj-color2" title="Sign in" id="collasible-nav-dropdown">
-                                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                                    <NavDropdown.Divider />
-                                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                                </NavDropdown></Nav.Link>
-                                                <Nav.Link eventKey={2} href="#memes" className="adj-color2" >
-                                                    <NavDropdown title="Find My Trips" >
-                                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                                        <NavDropdown.Divider />
-                                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                                    </NavDropdown>
-                                                </Nav.Link>
+
+                                                <Row>
+                                                    <Col md={3} className="adj-icon-col">
+                                                        <div className="adj-icon">
+                                                            <FontAwesomeIcon icon={faUser} />
+                                                        </div>
+                                                    </Col>
+                                                    <Col md={9} className="adj-icon-col-left">
+                                                        <Nav.Link href="#deets">  <NavDropdown className="adj-color2" title="Sign in" id="collasible-nav-dropdown">
+
+
+                                                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                                            <NavDropdown.Divider />
+                                                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+
+
+                                                        </NavDropdown></Nav.Link>
+                                                    </Col>
+                                                </Row>
+
+                                                <Row>
+                                                    <Col md={3} className="adj-icon-col">
+                                                        <div className="adj-icon">
+                                                            <FontAwesomeIcon icon={faLuggageCart} />
+                                                        </div>
+                                                    </Col>
+                                                    <Col md={9} className="adj-icon-col-left">
+                                                        <Nav.Link eventKey={2} href="#memes" className="adj-color2" >
+                                                            <NavDropdown title="Find My Trips" >
+                                                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                                                <NavDropdown.Divider />
+                                                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                                            </NavDropdown>
+                                                        </Nav.Link>
+                                                    </Col>
+                                                </Row>
+
                                                 {/*<Nav.Link eventKey={2} href="#memes">*/}
                                                 {/*    <NavDropdown title="Help" id="coll">*/}
                                                 {/*        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
@@ -81,15 +108,26 @@ class PriceLine extends React.Component {
                                                 {/*</Nav.Link>*/}
                                             </Nav>
                                         </Navbar.Collapse>
-                                        <Nav.Link eventKey={2} href="#memes">
-                                            <NavDropdown title="Help" id="coll">
-                                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                                <NavDropdown.Divider />
-                                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                            </NavDropdown>
-                                        </Nav.Link>
+
+                                        <Row>
+                                            <Col md={3} className="adj-icon-col adj-icon-col-left_ml">
+                                                <div className="adj-icon">
+                                                    <FontAwesomeIcon icon={faQuestionCircle} />
+                                                </div>
+                                            </Col>
+                                            <Col md={9} className="adj-icon-col-left ">
+                                                <Nav.Link eventKey={2} href="#memes">
+                                                    <NavDropdown title="Help" id="coll">
+                                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                                        <NavDropdown.Divider />
+                                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                                    </NavDropdown>
+                                                </Nav.Link>
+                                            </Col>
+                                        </Row>
+
                                     </Navbar>
                                 </Col>
                             </Row>
@@ -111,9 +149,7 @@ class PriceLine extends React.Component {
                     <Row>
                         <Col className="adj-col">
 
-                            <div>
-                                <FontAwesomeIcon icon={faHome} />
-                            </div>
+
                         </Col>
                     </Row>
 
