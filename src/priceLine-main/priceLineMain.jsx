d3dc8f8696dd5588167c5075480329217a08e04c
adringue <adrien.nguesseu@gmail.com>
2020-03-02 21:11:48 -0800

    added tabs and tab-navbar components

--- src/priceLine-main/priceLineMain.jsx
+++ src/priceLine-main/priceLineMain.jsx
@@ -1,44 +1,36 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-import {Container} from "react-bootstrap";
-import {Row} from "react-bootstrap";
-import {Col} from "react-bootstrap";
-import {Form} from "react-bootstrap";
-import {Button} from "react-bootstrap";
-import "./priceLineMain.css"
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import "./priceLineMain.css";
+
+import Header from '../head/header';
+import PriceLineTabs from '../priceline-tabs/priceline-tabs';
 
 class PriceLineMain extends React.Component {
-
     constructor() {
         super();
-
     }
-
-
     render = () => {
-
-
         return (
             <div className="">
-                <Container>
+                <Container className="main-container">
+                   
+                                    <div className="adj_new_2  main">
+                                        <Header />
+                                        <PriceLineTabs />
+                                    </div>
+                                                                 
 
-                    <Row>
-                        <Col>
-                            <div className="adj_new">
-                                </div>
-
-
-
-                        </Col>
-                    </Row>
 
 
                 </Container>
-
             </div>
         );
     }
-
 };
 
 export default PriceLineMain;
